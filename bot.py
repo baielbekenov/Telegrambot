a3df1a5389c5c5f54d3dfc46df7b87928ef0d66f
user <baelbekbolotovb12356@gmail.com>
2022-02-14 22:20:02 +0600

    modified

--- bot.py
+++ bot.py
@@ -14,16 +14,24 @@ def welcome(message):
     item1 = types.KeyboardButton("Рандомное число")
     item2 = types.KeyboardButton("Как дела?")
 
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    audio1 = types.KeyboardButton("Music1")
+    audio2 = types.KeyboardButton("Music2")
+    audio3 = types.KeyboardButton("Music3")
+    markup.add(audio1, audio2, audio3)
+
+
+
     markup.add(item1, item2)
     bot.send_message(message.chat.id, f"Добро пожаловать {message.from_user.username}"
-                                      f"я бот созданный чтобы отвечать на ваши вопросы! \U0001F604", parse_mode='html', reply_markup=markup)
+                                      f" я бот созданный чтобы отвечать на ваши вопросы! \U0001F604", parse_mode='html', reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
 def lalala(message):
     if message.chat.type == 'private':
-        if message.text == 'Рандомное число':
+        if message.text.lower() == 'Рандомное число'.lower():
             bot.send_message(message.chat.id, str(random.randint(0, 100)))
-        elif message.text == 'Как дела?':
+        elif message.text.lower() == 'Как дела?'.lower():
 
             markup = types.InlineKeyboardMarkup(row_width=2)
             item1 = types.InlineKeyboardButton("Хорошо", callback_data='good')
@@ -32,25 +40,36 @@ def lalala(message):
             markup.add(item1, item2)
 
             bot.send_message(message.chat.id, 'Отлично сам как?', reply_markup=markup)
-        elif message.text == 'Какие планы на выходные?':
+        elif message.text.lower() == 'Какие планы на выходные?'.lower():
             bot.send_message(message.chat.id, "Я не знаю! Что предложишь? \U0001F64B")
-        elif message.text == 'Может в горы?':
+        elif message.text.lower() == 'Может в горы?'.lower():
             bot.send_message(message.chat.id, "Нее, я уже сходил!")
             bot.send_message(message.chat.id, "Давай в клуб!")
-        elif message.text == 'Расскажи анекдот!':
+        elif message.text.lower() == 'Расскажи анекдот'.lower():
             bot.send_message(message.chat.id, "Возвращается домой пьяный мужик. Дверь открывает жена. Тут мужик берет и захлопывает дверь:"
                                                 "— Откуда пришла — там и ночуй! \U0001F602")
+        elif message.text.lower() == 'Music3'.lower():
+            audio = open('music/the-weeknd-blinding-lights.mp3', 'rb')
+            bot.send_audio(message.chat.id, audio)
+        elif message.text.lower() == 'Music2'.lower():
+            audio = open('music/The Weeknd — Devil May Cry.mp3', 'rb')
+            bot.send_audio(message.chat.id, audio)
+        elif message.text.lower() == 'Music1'.lower():
+            audio = open('music/The Weeknd — Faith.mp3', 'rb')
+            bot.send_audio(message.chat.id, audio)
         else:
-            bot.send_message(message.chat.id, 'Я не знаю что ответить! \U0001F609')
+            bot.send_message(message.chat.id, 'Я не понимаю тебя! Пожалуйста \U0001F64F пиши разборчиво! \U0001F609')
+
+
 
 @bot.callback_query_handler(func=lambda call:True)
 def callback_inline(call):
     try:
         if call.message:
             if call.data == 'good':
-                bot.send_message(call.message.chat.id, 'Каааайф')
+                bot.send_message(call.message.chat.id, 'Правда? завидую!')
             elif call.data == 'bad':
-                bot.send_message(call.message.chat.id, 'Ну фигово! справляйся!')
+                bot.send_message(call.message.chat.id, 'Ну удачи! справляйся!')
 
             #remove inline buttons
             bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Как дела?",
